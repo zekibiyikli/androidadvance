7f44c9097a98397d967bbde9f498d1f6055484be
Zekeriya Bıyıklı <zbiyikli1@gmail.com>
2022-03-06 20:18:29 +0300

    Other - Lottie

--- app/src/main/java/com/example/study/other/OtherActivity.kt
+++ app/src/main/java/com/example/study/other/OtherActivity.kt
@@ -2,6 +2,9 @@ package com.example.study.other
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
 import com.bumptech.glide.Glide
 import com.example.study.R
 import com.example.study.databinding.ActivityLivedataBinding
@@ -12,21 +15,24 @@ class OtherActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityOtherBinding
     var imageUrl="https://i.pinimg.com/736x/d1/9e/64/d19e643478d30bd2dd039b363c8634cd.jpg"
+    lateinit var btnLottie:LottieAnimationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOtherBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_other)
 
+        btnLottie=findViewById(R.id.btnLottie)
+
         initGlide()
         initPicasso()
+        initLottie()
     }
 
     fun initGlide(){
         Glide.with(this)
             .load(imageUrl)
             .centerCrop()
-            .placeholder(R.drawable.ic_launcher_foreground)
             .into(binding.imgGLide)
     }
     fun initPicasso(){
@@ -36,6 +42,21 @@ class OtherActivity : AppCompatActivity() {
             .rotate(90F)
             .centerCrop()
             .placeholder(R.drawable.ic_launcher_foreground)
-            .into(binding.imgGLide)
+            .into(binding.imgPicasso)
+    }
+    fun initLottie(){
+        btnLottie.setAnimation("buynow.json")
+        btnLottie.setOnClickListener {
+            btnLottie.setMinProgress(0.2f)
+            btnLottie.setMaxProgress(0.5f)
+            btnLottie.repeatCount = LottieDrawable.INFINITE
+            btnLottie.playAnimation()
+
+            Handler().postDelayed({
+                btnLottie.repeatCount = 1
+                btnLottie.setMaxProgress(1.0f)
+
+            }, 3000)
+        }
     }
 }
