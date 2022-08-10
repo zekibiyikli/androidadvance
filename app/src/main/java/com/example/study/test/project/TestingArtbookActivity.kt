a5ce360fe6b52c91abc5982a0fe1a2bbebab0bc4
Zekeriya Bıyıklı <zbiyikli1@gmail.com>
2022-08-10 22:38:14 +0300

    Test => 2

---
+++ app/src/main/java/com/example/study/test/project/TestingArtbookActivity.kt
@@ -0,0 +1,12 @@
+package com.example.study.test.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.study.R
+
+class TestingArtbookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testing_artbook)
+    }
+}
